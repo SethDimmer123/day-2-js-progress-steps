3ea13a123dd70252f092ccedf756bc4118d155ac
Seth DImmer <sethdimmer1@gmail.com>
2023-03-09 09:52:10 -0500

    initial commit

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-next.addEventListener('click',()=>{
+next.addEventListener('click', function(){
     currentActive++
-    // increases in increments of 1
+        // increases in increments of 1
 
     if(currentActive > circles.length) { // length means the amount of circles
         // if currentAtive is greater than
@@ -18,37 +18,34 @@ next.addEventListener('click',()=>{
 
     update()
 })
-prev.addEventListener('click',() =>{
-    currentActive--
-    // decreases in increments of 1
 
-    if(currentActive > 1) { // length means the amount of circles
-        // if currentAtive is less than 1
-        currentActive = 1 // i do not want to go under 1
+prev.addEventListener('click', () => {
+    currentActive--
+     // decreases in increments of 1
+
+    if(currentActive < 1) {// length means the amount of circles
+                // if currentActive is less than 1
+        currentActive = 1// i do not want to go under 1
     }
     // console.log(currentActive)
     // now the decrements only go to 1.
 
     update()
 })
-
-
 // updates the DOM
 
 function update() {
-    circles.forEach(function(circle,index){
-        if(index < currentActive) { // if the index of the particular 
-            // circle is less than the current active.
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {// if the index of the particular 
             circle.classList.add('active')
-        }
-        else{
+        } else {
             circle.classList.remove('active')
         }
     })
     // get all of the active circles
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%'
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 // accessing the progress style and width
 // we add percentages for the css width property
 // we add the -1 to give me a lower percentage to match where i want the blue 
@@ -56,31 +53,13 @@ function update() {
 
 // working on previous button
 
-if(currentActive === 1) {
-    prev.disabled = true
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
-else if(currentActive === circles.length) { // if the currentActive is at the end 
-    // of the circles length
-    // then i disable the next button.
-    next.disabled = true
-}
-else{
-    prev.disabled = false
-    next.disabled = false
-}
-}
-
-// function update() {
-//     circles.forEach((circle,index) => {
-//         if(index < currentActive) { // if the index of the particular 
-//             // circle is less than the current active.
-//             circle.classList.add('active')
-//         }
-//         else{
-//             circle.classList.remove('active')
-//         }
-//     })
-
-// }
-// arrow function same result
 
